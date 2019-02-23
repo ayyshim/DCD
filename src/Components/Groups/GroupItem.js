4ab608866a6728da754f8ecb845dbb4a9f2967f3
kanxuravi <kanxuravi9680@gmail.com>
2019-02-23 18:50:45 +0530

    14

--- src/Components/Groups/GroupItem.js
+++ src/Components/Groups/GroupItem.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import {Menu, Icon} from 'antd'
 
+const style = {
+  display: 'inline-block',
+  margin: '16px 32px 16px 0',
+};
+
+
 const GroupItem = (props) => (
-    <Menu.Item key={props.group.id}
+  <Menu.Item key={props.group.id} id="li-hover"
         onClick={props.onClick}
         onMouseEnter={_=>_}
         onItemHover = {_=>_}
