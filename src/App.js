3e2e0e264bbde429166561e37f646c5d7ae451f3
vijay kumar <vickyk25021997@gmail.com>
2019-02-23 18:46:09 +0530

    integrated code editor

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import LoginScreen from './Components/Screens/Login';
 import DashboardScreen from './Components/Screens/Dashboard';
+import Test from './Components/Screens/test';
 class App extends Component {
 
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={LoginScreen}/>
             <Route exact path="/dashboard" component={DashboardScreen}/>
+            <Route exact path="/test" component={Test}/>
           </Switch>
         </div>
       </BrowserRouter>
